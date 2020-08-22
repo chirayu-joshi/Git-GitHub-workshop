f6a5f7adffe89ff8e1c4cac1e231e1dfb6c28aba
chirayu-joshi <46321376+chirayu-joshi@users.noreply.github.com>
2020-08-22 11:35:47 +0530

    Created calculator.cpp program

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int num1, int num2) {
+  return num1 + num2;
+}
+
+int main() {
+  
+  int num1, num2;
+  
+  cout << "Enter first number: ";
+  cin >> num1;
+  cout << "Enter second number: ";
+  cin >> num2;
+  
+  cout << "Addition of first number and second number is: " << add(num1, num2);
+  
+  return 0;
+}
